dbb88a9319f303a79438c0e66c3cc6f4019591c3
astratow <davno_pora@mail.com>
2022-01-23 06:25:56 +0000

    play around

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React from 'react'; // deprecated
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+function Greetings(){
+  return(
+    <section>
+      <Book />
+      <Image />
+      <Author/>
+      <Title/>
+    </section>
+  );
+}
+const Image = () => {
+  return <img src="https://images-na.ssl-images-amazon.com/images/I/41wLQIOffxL._SX411_BO1,204,203,200_.jpg" alt="book"/>;
+};
+const Book = () =>{
+  return <article>this is a book</article>;
+};
+const Author = () => {
+  return <h1>James Norbury </h1>;
+};
+
+const Title = () => {
+  return <article>Big Panda and Tiny Dragon</article>
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Greetings/>,
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
